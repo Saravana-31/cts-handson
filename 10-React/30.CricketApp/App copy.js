e4f6c9329226660c0645c1da3a1c9f4379829b1f
Saravana-31 <saravanap.23aim@kongu.edu>
2026-07-21 09:48:41 +0530

    push missed handson of week 6

---
+++ 10-React/30.CricketApp/App copy.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
+function App() {
+    const flag = false;
+    if (flag) {
+        return (
+            <div>
+                <ListofPlayers />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <IndianPlayers />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ 10-React/30.CricketApp/IndianPlayers copy.js
@@ -0,0 +1,46 @@
+import React from "react";
+function IndianPlayers() {
+    const team = [
+        "Sachin",
+        "Dhoni",
+        "Virat",
+        "Rohit",
+        "Rahul",
+        "Pant"
+    ];
+    const [odd1, even1, odd2, even2, odd3, even3] = team;
+    const T20players = [
+        "Surya",
+        "Hardik",
+        "Tilak"
+    ];
+    const RanjiPlayers = [
+        "Pujara",
+        "Rahane",
+        "Saha"
+    ];
+    const mergedPlayers = [...T20players, ...RanjiPlayers];
+    return (
+        <div>
+            <h2>Odd Team Players</h2>
+            <ul>
+                <li>{odd1}</li>
+                <li>{odd2}</li>
+                <li>{odd3}</li>
+            </ul>
+            <h2>Even Team Players</h2>
+            <ul>
+                <li>{even1}</li>
+                <li>{even2}</li>
+                <li>{even3}</li>
+            </ul>
+            <h2>Merged Team</h2>
+            <ul>
+                {mergedPlayers.map((player, index) => (
+                    <li key={index}>{player}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default IndianPlayers;
